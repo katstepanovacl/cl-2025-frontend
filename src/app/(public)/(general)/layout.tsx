90503b687109fc3b0c361d74729fd49ffd37e466
Katerina Stepanova <admin@curatelocal.com>
2025-09-25 19:38:38 -0400

    setting up globals

---
+++ src/app/(public)/(general)/layout.tsx
@@ -0,0 +1,7 @@
+import { ReactNode } from "react";
+
+const GeneralLayout = ({ children }: { children: React.ReactNode }) => {
+  return <div>{children}</div>;
+};
+
+export default GeneralLayout;
---
+++ src/app/(public)/(general)/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
+};
+
+export default HomePage;
